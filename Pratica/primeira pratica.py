5aacf142f6bdc04de943680a44a0e197463d4692
unknown <l.lilinaipe@hotmail.com>
2026-09-20 21:05:52 -0300

    first commit

---
+++ Pratica/primeira pratica.py
@@ -0,0 +1,7 @@
+print("seu nome é: "+ input("digite seu nome: "))
+
+nome = "olha meu nome"
+numero = 11
+flutuante = 12.2
+boleano = True
+
---
+++ exercicios/Primeiro exercicio.py
@@ -0,0 +1,8 @@
+nome = input("Digite seu nome: ")
+n1  = int(input("digite seu salario: "))
+n2  = float(input("digite o quanto voce ganhou de porcentagem: "))
+
+soma = 1000 + n1 * n2
+
+print("ola "+ nome +" seu bonus é de:", soma)
+
